0e2b5fe3297a4adb1f603ccc960d6bf2400a3017
rodrigo95bar <47338073+rodrigo95bar@users.noreply.github.com>
2019-05-08 21:58:19 -0300

    Add files via upload

---
+++ Fantasma/main.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "utn.h"
+#include "fantasma.h"  //cambiar por nombre entidad
+
+
+#define QTY_TIPO 3
+
+int main()
+{
+     myMenu();
+     return 0;
+}
